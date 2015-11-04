828f29ed713e6c86660b4bbeb48551c7a2771947
JVThomas <jthomas0209@gmail.com>
2015-11-04 11:32:21 -0500

    Done.

--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,30 @@
 # Enter your object-oriented solution here!
+class Multiples
+  def initialize(limit)
+    @numLimit = limit
+    @multiple_array = []
+  end
+
+  def collect_multiples()
+    counter = 1
+    while counter < @numLimit
+      if counter % 3 == 0 || counter % 5 == 0
+        @multiple_array.push(counter)
+      end
+      counter +=1
+    end
+    @multiple_array
+  end
+
+  def sum_multiples()
+    counter = 1
+    sum = 0
+    while counter < @numLimit
+      if counter % 3 == 0 || counter % 5 == 0
+        sum += counter
+      end
+      counter += 1
+    end
+    sum
+  end
+end
